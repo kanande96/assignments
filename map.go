9e36618af0b755121ee6176b560f57e6e0700687
kanande96 <deepkanan.96@gmail.com>
2022-02-17 13:25:38 -0600

    gh

---
+++ map.go
@@ -0,0 +1,13 @@
+package main
+
+import "fmt"
+
+func main() {
+	elements := make(map[string]string)
+	elements["H"] = "Hydrogen"
+	elements["He"] = "Helium"
+	elements["Li"] = "Lithium"
+	elements["Be"] = "Beryllium"
+	elements["B"] = "Boron"
+	fmt.Println(elements["Li"])
+}
